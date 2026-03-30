64fa4d13304ff21f3169424e0e47363d75cb8507
kishor g s <kishor.15082002@gmail.com>
2026-03-30 18:23:28 +0530

    learnt closure

--- closure.js
+++ closure.js
@@ -1,7 +1,7 @@
 //Closure is a machanism in which the inner function keeps the memory of the outer function even after the outer function execution stops
 function createCounter() {
     let count = 0;
-    return function(){
+    return function(){ // Here when the outer function executes the return statement it's execution stops 
         count++;
         console.log(count);
     };
