cecb7a26a17d0bb29d7ea8ca06f80c4dd8a6de4e
kishor g s <kishor.15082002@gmail.com>
2026-03-31 12:11:41 +0530

    solved the issue with delete button

--- CRUD app test/index.js
+++ CRUD app test/index.js
@@ -13,6 +13,12 @@ editButton.textContent = "Edit";
 deleteButton.textContent = 'Delete';
 viewButton.textContent = 'View';
 
+// I did not added this part because of this all the buttons used to add the data even when I click delete it was not deleting the data, why because since all the buttons were inside the form all the buttons used to fire the submit event but when I externally mentioned which button should fire the submit event it worked well
+// addButton.type = "submit";
+// editButton.type = "button";
+// viewButton.type = "button";
+// deleteButton.type = "button";
+
 const taskFormComponent = document.createElement('form');
 taskFormComponent.style.display = 'flex';
 taskFormComponent.style.flexDirection = 'column';
@@ -43,15 +49,14 @@ const showDataDiv = document.createElement('div');
 let data = '';
 
 editButton.addEventListener('click',()=>{
-    tasks.title =  taskNameInput;
-    tasks.description = taskDescriptionInput;
+    tasks.title =  taskNameInput.value;
+    tasks.description = taskDescriptionInput.value;
 
-    localStorage.setItem('tasks',tasks);
+    localStorage.setItem('tasks',JSON.stringify(tasks));
 });
 
 deleteButton.addEventListener('click',()=>{
     localStorage.removeItem('tasks');
-    console.log("Delete is clicked")
 });
 
 viewButton.addEventListener('click',()=>{
